939b4d0747c7392b86298d020b92ebe04ed2f8a1
Liam Ilan <snowboardsheep@gmail.com>
2020-03-17 18:13:58 -0700

    Updates to filtration when pulling stories.

--- generators/getstories.js
+++ generators/getstories.js
@@ -8,12 +8,13 @@ const MongoClient = Mongo.MongoClient
 const mongoURI = process.env.MONGODB_URI
 const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true }
 
+// db
 let db
 
 // connect
 MongoClient.connect(mongoURI, mongoOptions, (err, client) => {
   // handle errors
-  if (err) return console.log({ fail: 'database error' })
+  if (err) { return console.log(err) } else { console.log('connected, fetching data') }
 
   // get database
   const dbName = mongoURI.substr(mongoURI.lastIndexOf('/') + 1)
@@ -22,12 +23,22 @@ MongoClient.connect(mongoURI, mongoOptions, (err, client) => {
   // get stories
   db.collection('stories').distinct('text', {}, function (err, result) {
     // handle error
-    if (err) return { fail: 'database error' }
+    if (err) { console.log(err) } else { console.log('data received, writing to file') }
+
+    // data to write
+    const txt = JSON.stringify(Object.values(result).filter((item) => {
+      // filter length, and mentions of my name
+      // name because my class used my name a lot, and I don't want it to show up in the final generation
+      // if using yourself, please to customize this filter to your need
+      return item.length < 1000 && !item.includes('liam') && !item.includes('Liam')
+    }))
 
     // write to file data
-    fs.writeFile('generators/data.json', JSON.stringify(result), function (err) {
-      if (err) throw err
-      console.log('finished writing')
+    fs.writeFile('generators/data.json', txt, function (err) {
+      if (err) { throw err } else { console.log('finished writing') }
+
+      // close
+      client.close()
     })
   })
 })
